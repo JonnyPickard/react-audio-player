6b144ee89c822dd5e01cf11bf48216acdd4b9387
Jonny Pickard <Jonathandpickard@gmail.com>
2023-09-14 14:13:00 +0100

    Change vite base location to the react-audio-player repo

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/react-audio-player",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
