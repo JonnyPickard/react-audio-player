85ed67a6059021ea7b427622fb84dd15561bb882
Jonny Pickard <Jonathandpickard@gmail.com>
2023-09-18 17:46:47 +0100

    Removed theme provider solution as type support doesn't work as intended

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,22 +1,6 @@
-import { CSSProp } from "@emotion/react";
-
 declare module "howler" {
   interface Howl {
     _emit: (event: string) => void;
     seek(seek: number, id?: number): this | number;
-  }
-}
-
-declare module "react" {
-  interface DOMAttributes {
-    css?: CSSProp;
-  }
-}
-
-declare global {
-  namespace JSX {
-    interface IntrinsicAttributes {
-      css?: CSSProp;
-    }
   }
 }
